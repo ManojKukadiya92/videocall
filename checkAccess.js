cff06cbd65077e9c990bb60e446ab0f6f3135765
ManojKukadiya92 <63141010+ManojKukadiya92@users.noreply.github.com>
2023-09-04 13:06:09 +0530

    adding

--- checkAccess.js
+++ checkAccess.js
@@ -4,6 +4,7 @@ module.exports = () => {
   return (req, res, next) => {
     const token = req.body.key || req.query.key || req.headers.key;
     if (token) {
+      console.log('ok');
       if (token == config.secret_key) return next();
       else
         return res
